67ca0f67ae0becab83de750ec6734d0328d68968
Rafael Dias <s.rafaeldias@protonmail.com>
2023-10-09 16:05:17 +0200

    feat(stateful-streaming): add timestamp logic for max and min values

--- stateful-streaming/README.md
+++ stateful-streaming/README.md
@@ -9,3 +9,4 @@ python main.py
 ## References
 - https://www.waitingforcode.com/apache-spark-structured-streaming/arbitrary-stateful-processing-pyspark-applyinpandaswithstate/read
 - https://spark.apache.org/docs/latest/api//python/reference/pyspark.sql/api/pyspark.sql.GroupedData.applyInPandasWithState.html?highlight=applyinpandaswithstate#pyspark.sql.GroupedData.applyInPandasWithState
+- https://github.com/bartosz25/spark-playground/tree/master/pyspark-arbitrary-stateful-processing
--- stateful-streaming/main.py
+++ stateful-streaming/main.py
@@ -4,7 +4,6 @@ from pyspark import SparkConf
 import pandas as pd
 from typing import Iterable, Iterator
 from datetime import datetime
-from typing import List
 
 from pyspark.sql.streaming.state import GroupStateTimeout, GroupState
 
@@ -12,10 +11,6 @@ from pyspark.sql.streaming.state import GroupStateTimeout, GroupState
 def count_messages(
     key: tuple, data_iter: Iterable[pd.DataFrame], state: GroupState
 ) -> Iterator[pd.DataFrame]:
-    """
-    Count how many messages for each id has been processed during
-    the lifetime of the streaming job.
-    """
     # `key` will be a tuple of all group by keys
     # For example:
     # df.group_df("a") -> key = ("a",)
@@ -23,23 +18,43 @@ def count_messages(
 
     # Get the current state
     if state.exists:
-        (count, values) = state.get
+        (count, values, min_ts, max_ts) = state.get
     else:
         count = 0
         values = []
+        min_ts = datetime.now()
+        max_ts = datetime.now()
+
 
     for group_df in data_iter:
         # Count number of rows for the group by key (id)
         count += len(group_df)
         # Keep track of all `values` for an `id`
-        values += group_df["value"].to_list()
+        # values += group_df["value"].to_list()
 
-    new_state = (count, values)
+        # Keep track of min and max timestamps seen during the
+        # streaming job
+        # TODO: add watermark conditions here for time bound state
+        tmp_max_ts = group_df["ts"].max()
+        tmp_min_ts = group_df["ts"].min()
+
+        min_ts = min_ts if min_ts < tmp_min_ts else tmp_min_ts
+        max_ts = max_ts if max_ts > tmp_max_ts else tmp_max_ts
+
+    new_state = (count, values, min_ts, max_ts)
     state.update(new_state)
 
     # The yield DataFrame is **not** the state itself,
     # is just the output data from the streaming job
-    yield pd.DataFrame({"id": [str(key[0])], "count": [count], "values": [values]})
+    yield pd.DataFrame(
+        {
+            "id": [str(key[0])],
+            "count": [count],
+            "values": [values],
+            "min_ts": [min_ts],
+            "max_ts": [max_ts],
+        }
+    )
 
 
 spark_conf = (
@@ -63,7 +78,7 @@ with SparkSession.builder.config(conf=spark_conf).getOrCreate() as sc:
     # df = df.selectExpr("CAST(value AS STRING) as id", "current_timestamp() as t")
 
     df = sc.readStream.format("rate-micro-batch").options(**read_opts).load()
-    df = df.selectExpr("value", "value % 6 AS id")
+    df = df.selectExpr("value", "value % 6 AS id", "current_timestamp() as ts")
 
     df = (
         df
@@ -75,12 +90,16 @@ with SparkSession.builder.config(conf=spark_conf).getOrCreate() as sc:
                     T.StructField("id", T.StringType(), False),
                     T.StructField("count", T.LongType(), False),
                     T.StructField("values", T.ArrayType(T.LongType()), False),
+                    T.StructField("max_ts", T.TimestampType(), False),
+                    T.StructField("min_ts", T.TimestampType(), False),
                 ]
             ),
             stateStructType=T.StructType(
                 [
                     T.StructField("count", T.LongType(), False),
                     T.StructField("values", T.ArrayType(T.LongType()), False),
+                    T.StructField("max_ts", T.TimestampType(), False),
+                    T.StructField("min_ts", T.TimestampType(), False),
                 ]
             ),
             outputMode="update",
